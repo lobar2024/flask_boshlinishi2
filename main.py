a8a5975c77bbd62489f27f22cb2bfa930b324ac9
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-03-26 15:50:17 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/data')
+def datalar():
+    my_list = list(range(1, 11))
+    return render_template('data.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
